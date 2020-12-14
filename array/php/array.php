9b49efe39034746d9382cc45208d326e1f634caf
Sugar0106 <1019252663@qq.com>
2020-12-14 17:12:44 +0800

    数组php实现

--- array/php/array.php
+++ array/php/array.php
@@ -1,106 +1,121 @@
 <?php
+    
 /**
- *数组数据结构php实现
+ *数据结构数组 php实现
  */
 class myarray{
+
     private $length;
     private $capacity;
     private $data;
-    public function __construct($capacity) {
-        if($capacity <=0 ) {
-            return null;
+
+
+    /**
+     *初始化一个数组 指定内存空间
+     */
+    public function __construct($capacity){
+        if($capacity < 0) {
+            return false;
         }
-        $this->length = 0;
         $this->capacity = $capacity;
         $this->data = array();
+        $this->length = 0;
     }
 
     /**
      *校验数组是否已满
+     *@return bool
      */
     private function checkIfFull() {
-        if($this->length >= $this->capacity) {
+        if($this->length == $this->capacity) {
             return true;
         }
         return false;
     }
 
     /**
-     *校验索引是否超出数组范围
+     *判断数组是否越界
+     *@param index 
+     *@return bool
      */
-     private function checkOutOfRange($index) {
-        if($index>=$this->length) {
+    private function checkIfOut($index) {
+        if($index>= $this->length) {
             return true;
-        }
+        } 
         return false;
-     }
+    }
 
     /**
-     *数组中插入元素 复杂度O(n)
+     *向数组中插入一个元素
+     *时间复杂度  O(n)
+     *@param index
+     *@param value
      */
-    function insertItem($index, $value) {
+    public function insertValue($index, $value) {
         $index = intval($index);
         $value = intval($value);
-        if($index < 0) {
+        if($index<0) {
+            return 0;
+        }
+        if($this->checkIfFull())  {
             return 1;
         }
-        if($this->checkIfFull()) {
-            return 2;
+        //将index后的数据后移
+        for($i = $this->length-1; $i>=$index; $i--) {
+            $this->data[$i+1] = $this->data[$i]; 
         }
-        for($i=$this->length-1; $i>=$index; $i--) {
-            $this->data[$i+1] = $this->data[$i];
-        } 
         $this->data[$index] = $value;
         $this->length++;
-        return [$this->data, $this->length, $this->capacity];
+        return $this->data;
     }
-
     /**
-     *数组中删除元素 复杂度O(n)
+     *删除数组元素
+     *时间复杂度 O(n)
+     *@param index
      */
-    function deleteItem($index) {
-        $index = intval($index);
-        if($index < 0){
+    public function deleteItem($index) {
+        if($index<0) {
+            return 0;
+        }
+        if($this->checkIfOut($index)) {
             return 1;
         }
-        if($this->checkOutOfRange($index)) {
-            return 2;
-        }
-        $value = $this->data[$index];
         for($i = $index; $i<$this->length-1; $i++) {
-            $this->data[$i]  = $this->data[$i+1];
+            $this->data[$i] = $this->data[$i+1];
         }
         $this->data[$this->length-1] = null;
         $this->length--;
-        return [$this->data, $this->length];
+        return $this->data;
     }
 
     /**
-     *数组中查询元素 复杂度O(1)
+     *查找数组元素
+     *@param index
      */
-    function searchItem($index) {
-        $index = intval($index);
+    public function findItem($index) {
         if($index<0) {
+            return 0;
+        }
+        if($this->checkIfOut($index)) {
             return 1;
-        }
-        if($index>$this->length-1) {
-            return 2;
-        }
-        return $this->data[$index];
+        } 
+        $data = $this->data[$index];
+        return $data;
     }
+
 }
 
-$obj = new myarray(10);
-
-//数组中插入数据
-for($i=0; $i<=9; $i++) {
-    list($data, $length, $capacity) = $obj->insertItem($i, $i);
+$arrObj = new myarray(10);
+//测试插入数据
+for($i=0; $i<11; $i++) {
+    $arr = $arrObj->insertValue($i, $i);
 }
 
-//数组中删除数据
-list($data, $length) = $obj->deleteItem(5);
-var_dump($data, $length);exit;
+//测试删除数据
+$data = $arrObj->deleteItem(3);
+var_dump($data);
 
-//数组中查询数据
-	
+//测试查找元素
+$item = $arrObj->findItem(5);
+var_dump($item);exit;
 
