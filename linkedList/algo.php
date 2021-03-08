c73199733ea16464aa00108e695d837596545d43
Sugar0106 <1019252663@qq.com>
2021-03-08 10:44:48 +0800

    链表实现算法

--- linkedList/algo.php
+++ linkedList/algo.php
@@ -12,7 +12,6 @@ function reverse($list) {
     $curNode = $list->head->next;
     $list->head->next = null;//断开头节点
     while($curNode != null) {
-        var_dump($curNode);
         $nextNode = $curNode->next; 
         $curNode->next = $preNode;
         $preNode = $curNode;
@@ -22,12 +21,49 @@ function reverse($list) {
     return true;
 }
 
+/**
+ *判断是否有环
+ */
+function checkCircle($list) {
+    if($list == null || $list->head == null || $list->head->next == null) {
+        return false;
+    }
+    $slow = $list->head->next;
+    $fast = $list->head->next;
+    while($fast != null && $fast->next != null) {
+        $fast = $fast->next->next;
+        $slow = $slow->next;
+        if($fast == $slow) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ *判断回文
+ */
+function checkHuiwen($list) {
+    if($list->length <= 1) {
+        return true;
+    }
+
+}
+
+//======================TEST================================
 $list = new SingleLinkedList();
 $list->insertFoot(1);
 $list->insertFoot(2);
 $list->insertFoot(3);
 $list->insertFoot(4);
 $list->insertFoot(5);
-var_dump($list);
+//反转链表
 reverse($list);
-var_dump($list);
+//制造一个有环链表
+$curNode = $list->head->next;
+while($curNode->next != null) {
+    $curNode = $curNode->next;
+}
+$curNode->next = $list->head->next;
+//判断链表是否有环
+var_dump(checkCircle($list));
